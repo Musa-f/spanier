8379fe6e3bad5b858948d2e58bde3c10f65a3b83
Musa-f <musaevafatima77@gmail.com>
2022-10-21 15:56:40 +0200

    panier

--- panier.php
+++ panier.php
@@ -7,16 +7,6 @@ include 'header.php'; ?>
     $ids = array_keys($_SESSION['panier']);
     //$products = $DB->query('SELECT * FROM produits WHERE id IN implode($ids)');
     ?>
-    <div id="articles">
-    <div class="container">
-                <div class="img-aliments"><img src="produits\gouda.png" alt="gouda"></div>
-                <div class="container-child">
-                    <h3>Gouda</h3>
-                    <div class="affiche"><strong class="prix" id="">5</strong> <span>€</span></div>
-                    <button>+</button>
-                </div>
-            </div>
-    </div>
 
     <table>
         <tr>
@@ -28,7 +18,7 @@ include 'header.php'; ?>
         </tr>
         <!--Ajouts de produits-->
         <tr> 
-            <td><img src="" alt=""></td>
+            <td><img src="" alt="" class="mini"></td>
             <td>Gouda</td>
             <td>5€</td>
             <td>2</td>
